fc8592baae03a98546ae3f4af04792bc31b6f08d
sponcey <spencer@charlesandthorn.com>
2018-10-03 16:59:19 -0400

    First stab at diacritic characters

--- documentation/scripts/GlyphConstruction_Generator.py
+++ documentation/scripts/GlyphConstruction_Generator.py
@@ -1,27 +1,9 @@
-# GOAL: Create script that outputs Glyph Construction file based on specific list.
-# METHOD: Using PLUS_Drawn as base, and PLUS_composites as final output, output list as text file in Glyph Construction language.
-    ## capitals
-    # "base_comb_glyph = base_glyph + comb_glyph.cap@hposition,vposition |univalue"
-    ## lowercase
-    # "base_comb_glyph = base_glyph + comb_glyph@hposition.vposition |univalue"
+## TO DO:
+    # Make list of exceptions for so that list only includes composite glyphs.
+    # Exceptions: base glyphs, punctuation
+    # Additional exception: replace i with dotlessi
 
-# Google Plus List:
-    # Base Glyphs Sublist
-        # Capitals Sublist
-        # Lowercase Sublist
-        # Smallcaps Sublist
-    # Combining Accents Glyphs Sublist
-        # Above Glyphs
-        # Below Glyphs
-    # Combined Glyphs Sublist (Final List?)
-        
-# FIRST: Identify comb_glyph in PLUS_Drawn, label vposition
-# SECOND: Sort PLUS_composites list for capitals and lowercase.
-# THIRD: Identify base_glyph in PLUS_Drawn, lable hposition
-# FOURTH: Output list formatted in Glyph Construction Language
-
-# Create lists from external text files
-# Retain Unicode values for comb_glyphs
+# Reading of text files, and converts them into usable lists.
 
 with open('glyphSorting/PLUS_composites_360.txt', 'r', encoding='latin1') as text2:
     # Composite Glyphs
@@ -54,19 +36,18 @@ with open('glyphSorting/PLUS_either_19.txt', 'r', encoding='latin1') as text3:
         linesplit = line.strip().rpartition(' ')
         PlusEith.append(linesplit[2])
 
-# Define dictionaries for accents
-# Need to add horizontalval and unicodeval to dictionary values
+# Looks at PlusDrawn list, and creates a dictionary that tags with "top" and "bottom" values
 
 accent_dict = {}
 
-for i in PlusDrawn[:119]:
+for i in PlusDrawn[:118]:
     if ".case" in i:
         continue
     else:
         if "comb" in i:
             i = i[:-4]
             accent_dict[i] = "top"
-for i in PlusDrawn[120:]:
+for i in PlusDrawn[118:]:
     if ".case" in i:
         continue
     else:
@@ -77,90 +58,54 @@ for i in PlusDrawn[120:]:
 accentsDictstoGlyphCon = []
 accentsDictstoGlyphCon_ignored = []
 
-def accentsDictsGenerator(glyphs, accents):
-    for accent in accents.keys():
-        for glyph in glyphs:
-            # if the base glyph is in the combining glyph and some other conditions:
-            if accent in glyph and len(accent) < len(glyph) and glyph not in str(accentsDictstoGlyphCon): # any of the lines in accentsDictstoGlyphCon:
-                # format this string and add the base glyph
-                accentsDictstoGlyphCon.append("%s = %s + %scomb@%s,%s | %s" % (glyph, glyph[:1], accent, "center", accents.get(accent), glyphs.get(glyph)))
-            # if the the base glyph already exists as a line in accentsDictstoGlyphCon:
-            if accent in glyph and any(line.startswith(glyph) for line in accentsDictstoGlyphCon):
-                # find that line, insert formatted string before " |"
-                line.replace(" |", "+ %scomb@%s,%s |" % (accent, "center", accents.get(accent))) #the string that begins with glyph
-        # how do I sort the missing 62 glyphs to the accentsDictstoGlyphCon_ignored?
-
-# I need a new function that parses the glyph, instead of comparing the accents to the glyph.
-def accentsDictsGenerator2(glyphs, accents):
-    for glyph in glyphs:
+# Function that takes a list of Composite Glyphs, list of Base Glyphs, and an Accent Dictionary (which has values for the position of the accent), and formats them
+# in a way that is recognizable for GlyphConstruction in Robofont.
+def accentsDictsGenerator(compglyphs, baseglyphs, accentsdict):
+    for glyph in compglyphs:
         glyphparsed = []
-        exceptions = ["AE", "DZ", "OE", "ae", "DZ", "oe"]
-        # define the base glyph
-        for line in PlusDrawn:
-            if glyph[:2] == line:
-                glyphparsed.append(glyph[:2])
-            elif glyph[:1] == line:
-                glyphparsed.append(glyph[:1])
-            # append that to the glyphparsed list
-        # for every accent in the glyph
-        ## accents need to be in order listed in glyph!
-        for accent in accents.keys():
-            if accent in glyph:
-                glyphparsed.append("%scomb@%s,%s" % (accent, "center", accents.get(accent)))
-        # using the glyphparsed list, compile and format a line that gets added to accentsDictstoGlyphCon
-            #is +: the right way to concatenate?
-        # print(glyphparsed)
-        accentsDictstoGlyphCon.append("%s = %s + %s | %s" % (glyph, glyphparsed[0], " + ".join(glyphparsed[1:]), glyphs.get(glyph)))
-        
-# I need a new function that parses the glyph, instead of comparing the accents to the glyph.
-
-# Something is broken in this function! But something is maybe kind of working.
-def accentsDictsGenerator3(glyphs, accents):
-    for glyph in glyphs:
-        glyphparsed = []
-        # look and see if first 2 letters of glyph match any base
-        for base in PlusDrawn:
+        for base in baseglyphs:
+            if base[:1].isupper():
+                case = ".case"
+            else:
+                case = ""
+            #if glyph == accents or noncomb glyphs in PlusDrawn :
+                # don't run anything below here and move on to the next base glyph
             if glyph[:2] == base:
                 glyphparsed.append(glyph[:2])
                 subglyph = glyph[2:]
-# !! accent parsing isn't working here!!
-                while subglyph == True:
-                    counter = 0
-                    subglyph = glyph[:counter]
-                    for accent in accents.keys():
-                        if accent in subglyph:
-                            glyphparsed.append("%scomb@%s,%s" % (accent, "center", accents.get(accent)))
-                            glyph = glyph[len(accent):]
+                subglyphcounter = len(subglyph)
+                subglyph2counter = 0
+                while subglyph2counter <= subglyphcounter:
+                    subglyph2 = subglyph[:subglyph2counter]
+                    for accent in accentsdict.keys():
+                        if accent in subglyph2:
+                            glyphparsed.append("%s@%s,%s" % (accent+"%s" % (case), "center", accentsdict.get(accent)))
+                            subglyph = subglyph[len(accent):]
                         else:
                             continue
-                    counter += 1
-            if glyph[:1] == base:
+                    subglyph2counter += 1
+            elif glyph[:1] == base:
                 glyphparsed.append(glyph[:1])
                 subglyph = glyph[1:]
-# !! accent parsing isn't working here!!
-                while subglyph == True:
-                    counter = 0
-                    subglyph = glyph[:counter]
-                    for accent in accents.keys():
-                        if accent in subglyph:
-                            glyphparsed.append("%scomb@%s,%s" % (accent, "center", accents.get(accent)))
-                            glyph = glyph[len(accent):]
+                subglyphcounter = len(subglyph)
+                subglyph2counter = 0
+                while subglyph2counter <= subglyphcounter:
+                    subglyph2 = subglyph[:subglyph2counter]
+                    for accent in accentsdict.keys():
+                        if accent in subglyph2:
+                            glyphparsed.append("%s@%s,%s" % (accent+"%s" % (case), "center", accentsdict.get(accent)))
+                            subglyph = subglyph[len(accent):]
                         else:
                             continue
-                    counter += 1
+                    subglyph2counter += 1
             else:
                 continue
-        accentsDictstoGlyphCon.append("%s = %s + %s | %s" % (glyph, glyphparsed[0], " + ".join(glyphparsed[1:]), glyphs.get(glyph)))
+        accentsDictstoGlyphCon.append("%s = %s + %s | %s" % (glyph, glyphparsed[0], " + ".join(glyphparsed[1:]), compglyphs.get(glyph)))
 
+# Plug in list of composites, list of bases, and accent dictionary here.               
+accentsDictsGenerator(PlusCompDict, PlusDrawn, accent_dict)
 
-# Plug in list of composites and list of bases here                
-accentsDictsGenerator3(PlusCompDict, accent_dict)
-
+# Sorts and prints the list that accentsDictsGenerator makes.
 accentsDictstoGlyphCon.sort()
-
 for i in accentsDictstoGlyphCon:
     print(i)
-
-# print(len(accentsDictstoGlyphCon))
-# print(len(accentsDictstoGlyphCon_ignored))
-# print(len(PlusCompDict))
