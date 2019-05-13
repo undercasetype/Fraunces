9555e361ac623a8ffbbe66474a0d2d42d4368d9c
Spencer Charles <spencer@charlesandthorn.com>
2019-05-13 16:42:24 -0400

    rebuilt diacritics with more accurate anchors

---
+++ sources/compare_anchors.py
@@ -0,0 +1,7 @@
+f1_path = u"Fraunces_OpticalMax.ufo"
+f2_path = u"Fraunces_WeightMax.ufo"
+f3_path = u"Fraunces.ufo"
+f1 = OpenFont(f1_path, showUI=False)
+f2 = OpenFont(f2_path, showUI=False)
+f3 = OpenFont(f3_path, showUI=False)
+
