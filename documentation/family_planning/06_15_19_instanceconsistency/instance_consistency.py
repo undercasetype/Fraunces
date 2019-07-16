dbcb819c15638457c7da74be5811407bd3dd96ff
Spencer Charles <spencer@charlesandthorn.com>
2019-07-16 16:32:35 -0400

    Finished proof doc to look at consistency of instances

--- documentation/family_planning/06_15_19_instanceconsistency/instance_consistency.py
+++ documentation/family_planning/06_15_19_instanceconsistency/instance_consistency.py
@@ -1,8 +1,31 @@
-newPage("LetterLandscape")
+for a in range(1,9,1):
+    newPage("Letter")
+    con = 1.68
+    opticalsize = (80/9) * a
+    fontSize = 9
+    font("Recur Mono", fontSize)
+    text("opsz: %s" % round((opticalsize)), (50, 50))
+    for x in range(0,height()-100,int(height()/8)):
+        fontSize = 72
+        font("Fraunces", fontSize)
+        fontVariations(wght = x*con, opsz = opticalsize)
 
-fontSize = 144
+        text("bingo", (50,height()-x-100))
 
-font("./fonts/Fraunces-VF.ttf", fontSize)
-fontVariations(wght = 100, opsz = 36)
+        font("Fraunces Italic", fontSize)
+        fontVariations(wght = x*con, opsz = opticalsize)
 
-text("bingo", (200,200))
+        text("bingo", (275,height()-x-100))
+                
+        stroke(0,1,1)
+        hval = height()-x-100
+        print(hval)
+        line((50,hval), (475,hval))
+        line((50,hval+(fontSize*0.46)), (475,hval+(fontSize*0.46)))
+        fontSize = 9
+        stroke(None)
+        font("Recur Mono", fontSize)
+        text("weight: %s" % (x*con), (500, height()-x-100))
+
+saveImage("instanceconsistency.pdf")
+saveImage("instanceconsistency.gif")
