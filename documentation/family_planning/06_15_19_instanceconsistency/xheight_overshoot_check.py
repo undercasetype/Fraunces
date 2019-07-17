4c8748525145c0a430f53a6f975caeb9d0d64d10
Spencer Charles <spencer@charlesandthorn.com>
2019-07-17 16:56:10 -0400

    wrote another script to better check xheight italic against xheight roman

---
+++ documentation/family_planning/06_15_19_instanceconsistency/xheight_overshoot_check.py
@@ -0,0 +1,36 @@
+# Write a script to proof paragraphs of text. 
+
+# To Do:
+    # Proof Roman against Italic in a single line
+
+fnames = ["Fraunces", "Fraunces Italic", "Recur Mono"]
+textsample = "oingo boingo "
+
+margin = 50
+steps = 7
+sizeincrements = 72 / steps
+pages = 7
+
+for y in range(0,pages,1):
+    newPage("LetterLandscape")
+    pageHeight = height()-(margin*2)
+    boxMargin = (margin/2)
+    boxHeight = (pageHeight/steps) - boxMargin
+    boxWidth = width()-100
+    for x in range(0,steps,1):
+        fsize = 32
+        osize = (72/pages)*y+7
+        weight = (((1000+(1000/steps))/steps)*x)
+        font("Recur Mono", 9)
+        text("%s" % (round(weight)), (50, margin+(margin*(x/2))+(boxHeight*x)+boxMargin-10))
+        txt = FormattedString()
+        txt.fontVariations(opsz=osize, wght=weight)
+        txt.append(textsample, font = "Fraunces", fontSize = fsize , fill = (0))
+        txt.append(textsample, font = "Fraunces Italic", fontSize = fsize , fill = (0))
+        txt.append(textsample, font = "Fraunces", fontSize = fsize , fill = (0))
+        textBox(txt, (50, margin+(margin*(x/2))+(boxHeight*x)+boxMargin, boxWidth, boxHeight))
+        print(osize)
+        print(weight)
+        
+saveImage("xheight_overshoot_check.pdf")
+saveImage("xheight_overshoot_check.gif")
