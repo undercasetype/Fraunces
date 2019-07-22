0fbe90fa954c84eb2dc964d4dc4e8e284f861fe3
Spencer Charles <spencer@charlesandthorn.com>
2019-07-22 12:48:02 -0400

    modified proof doc

--- documentation/proofs/073019/Paragraph_Proofs.py
+++ documentation/proofs/073019/Paragraph_Proofs.py
@@ -4,12 +4,20 @@ now = datetime.datetime.now()
 newFileName = "xheight_overshoot_check" + now.strftime("%Y_%m_%d-%H_%M_%S")
 
 fnames = ["Fraunces", "Fraunces Italic", "Recur Mono"]
-textsample = "oingo boingo "
+textsample = "on no boingo gon on gin "
 
 margin = 50
 steps = 7
 sizeincrements = 72 / steps
-pages = 7
+pages = 10
+
+#def textGenerator():
+    # text = FormattedString()
+    # for 
+    # text.append("on no boingo gon gin ", font = fPick, fontSize = fSize, fill = 0)
+    # return text
+    
+#def boxGenerator():
 
 for y in range(0,pages,1):
     newPage("LetterLandscape")
@@ -18,4 +26,21 @@ for y in range(0,pages,1):
     boxWidth = (width()/2)- boxMargin
     leftBoxHeight = pageHeight - boxMargin
     rightBoxHeight = (pageHeight/2 - boxMargin)
-    for x in range()
+    
+    # Box 1
+    fontVariations(wght = 100*y, opsz = 72)
+    font(fnames[0], 72)
+    
+    textBox(textsample*100, (margin, margin, boxWidth-boxMargin, leftBoxHeight))
+    
+    # Box 2
+    fontVariations(wght = 100*y, opsz = 54)
+    font(fnames[0], 36)
+    
+    textBox(textsample*100, ((boxMargin*2) + boxWidth, margin+rightBoxHeight+boxMargin, boxWidth-margin, rightBoxHeight))
+    
+    # Box 3
+    fontVariations(wght = 100*y, opsz = 24)
+    font(fnames[0], 12)
+    
+    textBox(textsample*100, ((boxMargin*2) + boxWidth, margin, boxWidth-margin, rightBoxHeight))
