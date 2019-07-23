4d13d77b2137aff552647cb172f3683e37224ba2
Spencer Charles <spencer@charlesandthorn.com>
2019-07-23 17:30:44 -0400

    refined script to look at roman and italic combined in randomly generated paragraphs

--- documentation/proofs/073019/Paragraph_Proofs.py
+++ documentation/proofs/073019/Paragraph_Proofs.py
@@ -4,22 +4,31 @@ now = datetime.datetime.now()
 newFileName = "xheight_overshoot_check" + now.strftime("%Y_%m_%d-%H_%M_%S")
 
 fnames = ["Fraunces", "Fraunces Italic", "Recur Mono"]
-textsample = "on no boingo gon on gin "
+
+textsample = "urus taters thieves smeeth vases garottes aisha smartass busier brags bohea outer hate humuses fee sestetts frag sarges sae hoggs theists toea bee tit fiefs moria barabbas rhombus ras reversi bombs rhus ruffes highth tither messages aromas hobo musette ethos festive sabs maire mouse tavah grasser sargus amir evie sufism evovae semite vivaria rotters barra aerobe garb biremes burst rattier hoofer suavest tose boito toshes emeritus fughetta teemer simitar teff masorah behight bitte fibster titbits tomb iambists suber obviate bream harims teer fasters thars batts erose biotite isis shmooses semmit girt astomous thereof gaus grousers vee baht grosers urger suffers vasts shamers timist tamises sterve gorgeous brooses forums favour tutus traverse summa marriage vor tarries ruts asa them etherise mores thetis arbiters offeror remit gumshoe stot babi brassart stems aerobomb armet agoutas three ravager barterer basest mustees boffs begift merosome gushes trireme grogram behoof bigfeet tate ties bargees misuser heaths shastra ort burgoos offerers faitor goor moms toots guru boffo automats risers measures true grout misaim trema artists gooiest murther father fehm iotas sororise variate gossamer stime bass souses revues breves humous samoa gath forfairs shavers vug mig rathe rubefies mithraea maist boohoos beaths abioses sambas oarage brut tabret arroba"
 
 margin = 50
 steps = 7
 sizeincrements = 72 / steps
 pages = 10
+pLength = 200
 
-#def textGenerator():
-    # text = FormattedString()
-    # for 
-    # text.append("on no boingo gon gin ", font = fPick, fontSize = fSize, fill = 0)
-    # return text
+textsample = textsample.split(" ")
+
+# This function generates formatted strings by randomly picking from the two fonts list, using the size specified, and paragraph length specified.
+
+def textGenerator(fPick, fSize, pLength):
+    text = FormattedString()
+    for x in range(0, pLength, 1):    
+        text.fontVariations(opsz = fSize , wght = 100*y )
+        text.append(choice(textsample) + " ", font = choice(fPick[0:2]), fontSize = fSize, fill = 0)
+    return text
     
-#def boxGenerator():
 
 for y in range(0,pages,1):
+    # textsamplenew = ""
+    # for x in range(0,100,1):
+    #     textsamplenew += choice(textsample) + " "
     newPage("LetterLandscape")
     pageHeight = height()-(margin*2)
     boxMargin = (margin/2)
@@ -28,19 +37,13 @@ for y in range(0,pages,1):
     rightBoxHeight = (pageHeight/2 - boxMargin)
     
     # Box 1
-    fontVariations(wght = 100*y, opsz = 72)
-    font(fnames[0], 72)
-    
-    textBox(textsample*100, (margin, margin, boxWidth-boxMargin, leftBoxHeight))
+    opticalsize = 72
+    textBox(textGenerator(fnames, opticalsize, 200), (margin, margin, boxWidth-boxMargin, leftBoxHeight))
     
     # Box 2
-    fontVariations(wght = 100*y, opsz = 54)
-    font(fnames[0], 36)
-    
-    textBox(textsample*100, ((boxMargin*2) + boxWidth, margin+rightBoxHeight+boxMargin, boxWidth-margin, rightBoxHeight))
+    opticalsize = 36
+    textBox(textGenerator(fnames, opticalsize, 200), ((boxMargin*2) + boxWidth, margin+rightBoxHeight+boxMargin, boxWidth-margin, rightBoxHeight))
     
     # Box 3
-    fontVariations(wght = 100*y, opsz = 24)
-    font(fnames[0], 12)
-    
-    textBox(textsample*100, ((boxMargin*2) + boxWidth, margin, boxWidth-margin, rightBoxHeight))
+    opticalsize = 7
+    textBox(textGenerator(fnames, opticalsize, 200), ((boxMargin*2) + boxWidth, margin, boxWidth-margin, rightBoxHeight))
