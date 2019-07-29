91176b4de733147f37c388f602c0b1dce08bc41d
Spencer Charles <spencer@charlesandthorn.com>
2019-07-29 15:36:44 -0400

    added comparisons of new to old beta build in character_to_character script

--- documentation/proofs/073019/Character_To_Character.py
+++ documentation/proofs/073019/Character_To_Character.py
@@ -1,20 +1,22 @@
 import datetime
 
 now = datetime.datetime.now()
-newFileName = "spacingstrings" + now.strftime("%Y_%m_%d-%H_%M_%S")
+newFileName = "character_to_character" + now.strftime("%Y_%m_%d-%H_%M_%S")
 
 # Text from external source
 path = "chars_to_chars.txt"
 textstuff = open(path, "r", encoding="utf-8")
 textread = textstuff.read()
 textstuff.close()
+# textread = textread.split("\n")
+# textread = textread[1]
 
 # Variables
 fnames = ["Fraunces", "Fraunces Italic", "Recur Mono"]
 frauncesVals = listFontVariations(fnames[0])
 wghtMin = frauncesVals['wght']['minValue']
 wghtMax = frauncesVals['wght']['maxValue']
-opMin = frauncesVals['opsz']['minValue']
+opMin = frauncesVals['opsz']['minValue']+0.1
 opMax = frauncesVals['opsz']['maxValue']
 
 wghtVals = (wghtMin, wghtMax)
@@ -23,17 +25,63 @@ opVals = (opMin, opMax)
 margin = 50
 gutter = 25
 columns = (4,2)
-sizeincrements = 72
+rows = 3
 masters = len(wghtVals) + len(opVals)
 fSize = 72
+captionHeight = 24
+boxHeight = ((height()-(margin*2))/(rows)) - (gutter*(rows-1)) - captionHeight
 
+pages = 3
+opSzsteps = (opMax-opMin) / (pages-1)
+wghtSteps = (wghtMax-wghtMin) / (rows-1)
+
+# Roman and Italic
 for f in range(0,2,1):
-    for x in range(0,len(opVals),1):
+    # Steps for opsz
+    for x in range(0,pages,1):
         newPage("TabloidLandscape")
-        textread1 = textread
-        for y in range(0,len(wghtVals),1):
+        # Steps for wght
+        for y in range(0,rows,1):
             font(fnames[f], fSize)
-            fontVariations(opsz=x,wght=y)
-            while len(textread1) > 0:
-                textread1 = textBox(textread1, (margin,margin+height()/4*y,width()-margin*2,height()/4), align="center")
+            tracking(5)
+            opSz = opMin+(opSzsteps*y)
+            wgHt = wghtMin+(wghtSteps*x)
+            fontVariations(opsz = opSz,wght = wgHt)
+            textBox(textread, (margin,margin+(boxHeight*y),width()-margin*2,boxHeight), align = "center")
+            font(fnames[2],9)
+            tracking(None)
+            textBox("OpSz: %s, Wght: %s" % (opSz,wgHt), (margin,margin+(boxHeight*y)-boxHeight+captionHeight,width()-margin*2,boxHeight), align = "center")
+            
+# Old to New Comparison
+
+fontdict = {"Fraunces Beta Regular OpMin": (9.1,400), "Fraunces Beta Bold OpMin": (9.1,750), "Fraunces Beta Regular OpMax": (144,400), "Fraunces Italic Beta Regular OpMin": (9.1,400), "Fraunces Italic Beta Bold OpMin": (9.1,750), "Fraunces Italic Beta Regular OpMax": (144,400), }
+
+def drawOldtoNewComparison(style):
+    newPage("TabloidLandscape")
+    font(style, fSize)
+    tracking(5)
+    textBox(textread,(margin,margin, width()-(margin*2),boxHeight),align = "center")
+    font(fnames[2],9)
+    tracking(None)
+    textBox(style, (margin,margin-boxHeight+captionHeight,width()-margin*2,boxHeight), align = "center")
+    if "Fraunces Italic" in style:
+        font("Fraunces Italic", fSize)
+    else:
+        font("Fraunces", fSize)
+    tracking(5)
+    fontVariations(opsz = fontdict[style][0], wght = fontdict[style][1])
+    textBox(textread,(margin,margin+(boxHeight*y), width()-(margin*2),boxHeight),align = "center")
+    font(fnames[2],9)
+    tracking(None)
+    textBox("Fraunces %s" % (str(fontdict[style])), (margin,300,width()-margin*2,boxHeight), align = "center")
+    
+drawOldtoNewComparison("Fraunces Beta Regular OpMin")
+drawOldtoNewComparison("Fraunces Beta Bold OpMin")
+#drawOldtoNewComparison("Fraunces Beta Regular OpMax")
+drawOldtoNewComparison("Fraunces Italic Beta Regular OpMin")
+drawOldtoNewComparison("Fraunces Italic Beta Bold OpMin")
+drawOldtoNewComparison("Fraunces Italic Beta Regular OpMax")
+    
+            
+saveImage("PDFs/%s.pdf" % (newFileName))
         
