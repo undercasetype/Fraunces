e5573c8cf175f3dba7d1467b2f063fede3c840fb
Spencer Charles <spencer@charlesandthorn.com>
2019-08-02 15:17:26 -0400

    Generated new VF files with complete Italic caps in all masters, generated new proof docs

---
+++ documentation/proofs/080219/xheight_overshoot_check.py
@@ -0,0 +1,36 @@
+import datetime
+
+now = datetime.datetime.now()
+newFileName = "xheight_overshoot_check" + now.strftime("%Y_%m_%d-%H_%M_%S")
+
+fnames = ["Fraunces", "Fraunces Italic", "Recur Mono"]
+textsample = "hamburgefontsiv "
+
+margin = 50
+steps = 7
+sizeincrements = 72 / steps
+pages = 7
+
+for y in range(0,pages,1):
+    newPage("LetterLandscape")
+    pageHeight = height()-(margin*2)
+    boxMargin = (margin/2)
+    boxHeight = (pageHeight/steps) - boxMargin
+    boxWidth = width()-100
+    for x in range(0,steps,1):
+        fsize = 32
+        osize = (72/pages)*y+7
+        weight = (((1000+(1000/steps))/steps)*x)
+        font("Recur Mono", 9)
+        text("%s" % (round(weight)), (50, margin+(margin*(x/2))+(boxHeight*x)+boxMargin-10))
+        txt = FormattedString()
+        txt.fontVariations(opsz=osize, wght=weight)
+        txt.append(textsample, font = "Fraunces", fontSize = fsize , fill = (0))
+        txt.append(textsample, font = "Fraunces Italic", fontSize = fsize , fill = (0))
+        txt.append(textsample, font = "Fraunces", fontSize = fsize , fill = (0))
+        textBox(txt, (50, margin+(margin*(x/2))+(boxHeight*x)+boxMargin, boxWidth, boxHeight))
+        print(osize)
+        print(weight)
+        
+saveImage("PDFs/%s.pdf" % (newFileName))
+#saveImage("%s.gif" % (newFileName))
