4e880d78f142e6f5ff76c4c7f8633d0ee9a258d2
Spencer Charles <spencer@charlesandthorn.com>
2019-08-09 15:58:27 -0400

    generated new proof docs with Goofy axis

---
+++ documentation/proofs/080819/Paragraph_Proofs.py
@@ -0,0 +1,103 @@
+import datetime
+
+## Variables
+
+now = datetime.datetime.now()
+newFileName = "paragraph_proofs" + now.strftime("%Y_%m_%d-%H_%M_%S")
+
+fnames = ["Fraunces", "Fraunces Italic", "Recur Mono"]
+frauncesVals = listFontVariations(fnames[0])
+wghtMin = frauncesVals['wght']['minValue']
+wghtMax = frauncesVals['wght']['maxValue']
+opMin = frauncesVals['opsz']['minValue'] + 0.1
+opMax = frauncesVals['opsz']['maxValue']
+goofMin = frauncesVals['goof']['minValue']
+goofMax = frauncesVals['goof']['maxValue']
+
+margin = 50
+steps = 7
+sizeincrements = 72 / steps
+pages = 3
+goofSteps = 3
+pLength = 200
+textHamburgerfontsiv = "sampletext.txt"
+
+weightNum = 1000/pages
+
+## Functions
+
+# This function generates formatted strings by randomly picking from the two fonts (in this case Roman or Italic), using the size specified, and paragraph length specified.
+
+def textGenerator(textPath, fSize, goofNum, pLength, pageNum, fontPick=None, track=None, whatCase=None):
+    textstuff = open(textPath, "r", encoding="utf-8")
+    textsample = textstuff.read()
+    textstuff.close()
+    if whatCase == "upper":
+        textsample = textsample.upper()
+    elif whatCase == "title":
+        textsample = textsample.title()
+    textsample = textsample.split(" ")
+    text = FormattedString()
+    for x in range(0, pLength, 1):    
+        text.fontVariations(opsz = fSize , wght = pageNum, goof = goofNum )
+        if fontPick in fnames:
+            text.append(choice(textsample) + " ", font = fnames[fnames.index(fontPick)], fontSize = fSize, fill = 0)
+        elif fontPick == None:
+            text.append(choice(textsample) + " ", font = choice(fnames[0:2]), fontSize = fSize, fill = 0)
+    return text
+    
+# This function takes the FormattedStrings() and lays them out across pages.
+
+def textDrawings(textPath, case = None, fontPicker = None):
+    for g in (1,50,100):
+        for y in range(100,1001,450):
+            # textsamplenew = ""
+            # for x in range(0,100,1):
+            #     textsamplenew += choice(textsample) + " "
+            newPage("TabloidLandscape")
+            pageHeight = height()-(margin*2)
+            boxMargin = (margin/2)
+            boxWidth = (width()/2)- boxMargin
+            topBoxHeight = (pageHeight/1.5) - boxMargin
+            bottomBoxHeight = (pageHeight/2.5) - boxMargin
+            goofNumb = (g)
+    
+            # Draw caption
+    
+            font(fnames[2], 8)
+            text("Weight: %s, Goofy: %s" % (y, goofNumb), (50, height()-50))
+    
+            # Box 1
+            opticalsize = opMax
+            textBox(
+                textGenerator(textPath, opticalsize, goofNumb,200, y, fontPick = fontPicker, whatCase = case), 
+                (margin, margin+bottomBoxHeight, 
+                boxWidth*2, topBoxHeight))
+            text(
+                "OpSz: %s" % (opticalsize), 
+                (margin, margin+bottomBoxHeight+boxMargin*2-10))
+    
+            # Box 2
+            opticalsize = opMax/3
+            textBox(
+                textGenerator(textPath, opticalsize, goofNumb, 200, y, fontPick = fontPicker, whatCase = case), 
+                (margin, margin, boxWidth-margin, bottomBoxHeight))
+            text(
+                "OpSz: %s" % (opticalsize), 
+                (50,50))
+    
+            # Box 3
+            opticalsize = opMin
+            textBox(
+                textGenerator(textPath, opticalsize, goofNumb, 500, y, fontPick = fontPicker, whatCase = case), 
+                ((boxMargin*2) + boxWidth, margin+boxMargin, boxWidth-margin, bottomBoxHeight))
+            text("OpSz: %s" % (opticalsize), 
+            ((boxMargin*2) + boxWidth, margin))
+
+## Drawings
+
+textDrawings(textHamburgerfontsiv)
+textDrawings(textHamburgerfontsiv, case = "upper")
+textDrawings(textHamburgerfontsiv, case = "title")
+
+saveImage("PDFs/%s.pdf" % (newFileName))
