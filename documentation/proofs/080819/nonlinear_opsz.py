bd143eafa1fc1a56871ceb8e463f3f4efe7cd667
Spencer Charles <spencer@charlesandthorn.com>
2019-08-09 12:47:56 -0400

    updated images

--- documentation/proofs/080819/nonlinear_opsz.py
+++ documentation/proofs/080819/nonlinear_opsz.py
@@ -1,14 +1,14 @@
 steps = int((144-9)/30)
 
 for x in range(144,9,-steps):
-    newPage(500,500)
+    newPage(300,300)
     fill(1,1,1)
     rect(0,0, width(),height())
     fill(0,0,0)
-    font("Fraunces", 600)
-    fontVariations(opsz = x, wght = 1000)
-    text("n", (74,100))
+    font("Fraunces", 300)
+    fontVariations(opsz = x, goof = 1, wght = 1000)
+    text("n", (40,50))
     font("Recur Mono", 10)
-    text("OpSz: %s" % (x), (50,50))
+    text("OpSz: %s" % (x), (25,25))
     
-saveImage("linear.gif")
+saveImage("nonlinear.gif")
