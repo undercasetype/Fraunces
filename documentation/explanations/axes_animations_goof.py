999b2d17a58e47e944453362a5f1d42843c84c1a
Spencer Charles <spencer@charlesandthorn.com>
2019-08-13 13:48:21 -0400

    added new animations

--- documentation/explanations/axes_animations_goof.py
+++ documentation/explanations/axes_animations_goof.py
@@ -6,15 +6,21 @@ def easeInOutQuad(t):
         t = 2 - t
         return 1 - 0.5 * (t ** 2)
 
-for x in range(0,900,40):
-    percent = easeInOutQuad(x/900)
+maxNum = 100
+minNum = 1
+frames = 30
+
+steps = int((maxNum-minNum)/frames)
+
+for x in range(minNum,maxNum,steps):
+    percent = easeInOutQuad(x/maxNum)
     newPage(2000, 600)
     
     fill(1,1,1)
     rect(0,0,width(),height())
     fill(0,0,0)
 
-    fontVariations(opsz = 48, wght = (percent*900)+100, goof = 1)
+    fontVariations(opsz = 144, wght = 1000, goof = percent*maxNum)
 
     font("Fraunces", 200)
 
@@ -26,17 +32,17 @@ for x in range(0,900,40):
     
     font("Recur Mono", 20)
     
-    text("Weight: %s" % (int((percent*900)+100)), (10*2,10*2))
+    text("Goofy: %s" % (int(percent*maxNum)), (10*2,10*2))
     
-for x in range(900,0,-40):
-    percent = easeInOutQuad(x/900)
+for x in range(maxNum,minNum,-steps):
+    percent = easeInOutQuad(x/maxNum)
     newPage(2000, 600)
     
     fill(1,1,1)
     rect(0,0,width(),height())
     fill(0,0,0)
 
-    fontVariations(opsz = 48, wght = (percent*900)+100, goof = 1)
+    fontVariations(opsz = 144, wght = 1000, goof = percent*maxNum)
 
     font("Fraunces", 200)
 
@@ -48,6 +54,6 @@ for x in range(900,0,-40):
     
     font("Recur Mono", 20)
     
-    text("Weight: %s" % (int((percent*900)+100)), (10*2,10*2))
+    text("Goofy: %s" % (int(percent*maxNum)), (10*2,10*2))
     
-saveImage("weight_axis.gif")
+saveImage("goof_axis.gif")
