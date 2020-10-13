ef8d2b815240f889b210e1c727c44b394ef6e746
arrowtype <arrowtypeco@gmail.com>
2020-10-13 17:28:57 -0400

    build fonts with updated instance names

---
+++ mastering/googlefonts/gftools-packager-fraunces.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+
+gftools packager \
+    mastering/googlefonts/upstream.yaml \
+    /Users/stephennixon/type-repos/google-font-repos/fonts \
+    # --branch Fraunces 144pt ExtraSoft Black
