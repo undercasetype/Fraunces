f539053d439a53785dafd14d5e8afe310ae3a7af
arrowtype <arrowtypeco@gmail.com>
2020-10-15 17:32:46 -0400

    rebuild fonts with ligature carets

--- mastering/googlefonts/gftools-packager-fraunces.sh
+++ mastering/googlefonts/gftools-packager-fraunces.sh
@@ -3,6 +3,10 @@
 gftools packager \
     mastering/googlefonts/upstream.yaml \
     /Users/stephennixon/type-repos/google-font-repos/fonts \
-    # yes, update the files in fonts/ofl/fraunces
-    --force
-    # --branch Fraunces 144pt ExtraSoft Black
+    -fpy
+
+# gftools qa -f ~/type-repos/google-font-repos/fonts/ofl/fraunces/*.ttf --fontbakery -o ./mastering/checks # not wor
+
+# mkdir -p mastering/checks
+# fontbakery check-googlefonts ~/type-repos/google-font-repos/fonts/ofl/fraunces/Fraunces\[SOFT,WONK,opsz,wght\].ttf --ghmarkdown mastering/checks/Fraunces.checks.md
+# fontbakery check-googlefonts ~/type-repos/google-font-repos/fonts/ofl/fraunces/Fraunces-Italic\[SOFT,WONK,opsz,wght\].ttf --ghmarkdown mastering/checks/Fraunces-Italic.checks.md
